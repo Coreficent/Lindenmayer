cee713de665c94f39de2329f8692e3bf20a680c3
Stephen Xie <coreficent@gmail.com>
2020-11-22 10:38:42 -0500

    Clean up interface control.

--- Assets/Coreficent/Interface/InterfaceControl.cs
+++ Assets/Coreficent/Interface/InterfaceControl.cs
@@ -10,32 +10,11 @@
     {
         public void Render()
         {
-            Debug.Log("on restart");
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             Main.Root.Render();
-        }
-        public void SetPreset(string index)
-        {
-            //Main.Root.SetPreset(index);
         }
         public void SelectPreset(int index)
         {
             Main.Root.SetPreset(index);
-        }
-        public void SlideAngle(float angle)
-        {
-
-        }
-        public void SlideLength(float length)
-        {
-            
-        }
-        public void SlideThickness(float thickness)
-        {
-            if (Main.Root)
-            {
-                //Main.Root.SetThickness(thickness);
-            }
         }
     }
 }
